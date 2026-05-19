edefa74fc3e5129fcd6f3a79d6e60e1b96b0d8d4
eshtiak23 <eshtiak4099@gmail.com>
2026-05-19 21:50:12 +0600

    feat: implement authentication context with user registration, login, and profile management

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBAeG3j2EJw7_jYsh-TVfu1AVYeiWLyZUs",
+  authDomain: "ideavault-d14a3.firebaseapp.com",
+  projectId: "ideavault-d14a3",
+  storageBucket: "ideavault-d14a3.firebasestorage.app",
+  messagingSenderId: "499385759153",
+  appId: "1:499385759153:web:7878550aa0acad0736b990"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,11 +1,85 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+
+import { auth } from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
 
+const googleProvider = new GoogleAuthProvider();
+
 export default function AuthProvider({ children }) {
+
+  const [user, setUser] = useState(null);
+
+  const [loading, setLoading] = useState(true);
+
+  // register
+  const createUser = (email, password) => {
+    setLoading(true);
+
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  // login
+  const loginUser = (email, password) => {
+    setLoading(true);
+
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // google login
+  const googleLogin = () => {
+    setLoading(true);
+
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  // update profile
+  const updateUserProfile = (name, photo) => {
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photo,
+    });
+  };
+
+  // logout
+  const logoutUser = () => {
+    return signOut(auth);
+  };
+
+  // observer
+  useEffect(() => {
+
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+
+      setUser(currentUser);
+
+      setLoading(false);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+
+  }, []);
+
   const authInfo = {
-    user: null,
-    loading: false,
+    user,
+    loading,
+    createUser,
+    loginUser,
+    googleLogin,
+    logoutUser,
+    updateUserProfile,
   };
 
   return (
